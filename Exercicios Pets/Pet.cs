b4c839731761e88f3f68a2e335d606b4007da1fa
Filipe <Filipecasagrande1@gmail.com>
2025-04-21 13:10:50 -0300

    Exercicio Basico de Programação orientada a objetos animais

---
+++ Exercicios Pets/Pet.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercicios_Pets
+{
+    public class Pet
+    {
+
+        public string nome;
+        public string tipo;
+        public int idade;
+
+        public void Animal()
+        {
+            Console.WriteLine("informe o nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o tipo de animal");
+            tipo = Console.ReadLine();
+
+            Console.WriteLine("informe a idade do animal");
+            idade = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void exibirPet()
+        {
+            Console.WriteLine("Informaçoes");
+            Console.WriteLine($"nome do animal é: {nome} ");
+            Console.WriteLine($"Tipo de animal é: {tipo} ");
+            Console.WriteLine($"Idade do animal é: {idade} ");
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
--- Exercicios Pets/Program.cs
+++ Exercicios Pets/Program.cs
@@ -1,19 +1,32 @@
-﻿namespace Exercicios_Pets
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicios_Pets
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Pet animal1 = new Pet();
+            animal1.Animal();
+            Console.WriteLine("-------------------------------");
 
+            Pet animal2 = new Pet();
+            animal2.Animal();
 
+            Console.WriteLine("-------------------------------");
 
+            animal1.exibirPet();
+            animal2.exibirPet();
 
-
+            Console.ReadLine();
 
 
 
 
 
         }
+
     }
 }
